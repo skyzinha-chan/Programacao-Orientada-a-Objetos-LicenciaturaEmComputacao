3dfc21c8d4f526f0036f3540456b27e5d032cb2e
Talita M Marques <143131367+skyzinha-chan@users.noreply.github.com>
2025-01-28 22:24:05 -0400

    feat: adição de atividade conceitos de exceções

---
+++ trabalhandoComExcecoes/src/realizandoExcecoes.java
@@ -0,0 +1,12 @@
+public class realizandoExcecoes {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Olá, mundo!");
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        
+    }
+}
